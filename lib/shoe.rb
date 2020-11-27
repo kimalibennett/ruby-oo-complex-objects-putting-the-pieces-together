43f01c11fe5997b701dcffed0dc902bc8bfe0569
kimalibennett <aminbennett@gmail.com>
2020-11-27 00:42:06 -0600

    commit tree 54a1a1cac7a8460f022f0db9f39d9e4998a83d9a

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
-# Make your shoe class here!
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+    BRANDS = []
+     def initialize(brand)
+       @brand = brand
+       brands = @brand
+  
+          if  !(BRANDS.include?(brands)) then
+            BRANDS << brands
+          end
+    
+    end
+    def cobble
+      self.condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+    brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+        # brands.each do |brand|
+        #   Shoe.new(brand)
+        # end
+  my_shoe=Shoe.new(brands)
+  # my_shoe.length
+  
+  
